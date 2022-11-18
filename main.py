fa2547ce4da2dd4100c4fc09adea1b207b59cedb
unknown <davidsp302@gmail.com>
2022-11-18 02:05:34 -0500

    Primer commit del api

--- main.py
+++ main.py
@@ -1,121 +1,192 @@
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_cors import CORS
 import json
 from waitress import serve
-import  datetime
+import datetime
 import requests
 import re
 
-app=Flask(__name__)
+app = Flask(__name__)
 cors = CORS(app)
-from flask_jwt_extended import create_access_token, verify_jwt_in_request
-from flask_jwt_extended import get_jwt_identity
-from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager
-app.config["JWT_SECRET_KEY"]="super-secret" #Cambiar por el que se conveniente
+app.config["JWT_SECRET_KEY"] = "super-secret"
 jwt = JWTManager(app)
+
+########## METODO PARA LOGIN Y CREACION DE TOKEN ##########
 @app.route("/login", methods=["POST"])
 def create_token():
     data = request.get_json()
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    url=dataConfig["url-backend-security"]+'/usuarios/validar'
+    url = dataConfig["url-backend-security"]+'/usuarios/validar'
     response = requests.post(url, json=data, headers=headers)
     if response.status_code == 200:
         user = response.json()
         expires = datetime.timedelta(seconds=60 * 60*24)
-        access_token = create_access_token(identity=user, expires_delta=expires)
+        access_token = create_access_token(
+            identity=user, expires_delta=expires)
         return jsonify({"token": access_token, "user_id": user["_id"]})
     else:
         return jsonify({"msg": "Bad username or password"}), 401
 
+########## VALIDACION DE PERMISO ANTES DE CORREO EN ENDPOINT ##########
 @app.before_request
 def before_request_callback():
-    endPoint=limpiarURL(request.path)
-    excludedRoutes=["/login"]
+    endPoint = limpiarURL(request.path)
+    excludedRoutes = ["/login"]
     if excludedRoutes.__contains__(request.path):
         pass
     elif verify_jwt_in_request():
         usuario = get_jwt_identity()
-        if usuario["rol"]is not None:
-            tienePersmiso=validarPermiso(endPoint,request.method,usuario["rol"]["_id"])
+        if usuario["rol"] is not None:
+            tienePersmiso = validarPermiso(endPoint, request.method, usuario["rol"]["_id"])
             if not tienePersmiso:
                 return jsonify({"message": "Permission denied"}), 401
         else:
             return jsonify({"message": "Permission denied"}), 401
+
+########## CAMBIAN ID Y NOMBRES POR ? PARA ENVIAR EL PERMISO ##########
 def limpiarURL(url):
     partes = url.split("/")
     for laParte in partes:
         if re.search('\\d', laParte):
             url = url.replace(laParte, "?")
+        if len(partes) == 5:
+            url = url.replace(partes[4], "?")
+        if len(partes) == 6:
+            if partes[4] == "candidato":
+                url = url.replace(partes[4], "?")    
     return url
-def validarPermiso(endPoint,metodo,idRol):
-    url=dataConfig["url-backend-security"]+"/permisos-roles/validar-permiso/rol/"+str(idRol)
-    tienePermiso=False
+
+########## VALIDA SI EL ROL TIENE ASIGNADO UNA URL ##########
+def validarPermiso(endPoint, metodo, idRol):
+    url = dataConfig["url-backend-security"] + \
+        "/permisos-roles/validar-permiso/rol/"+str(idRol)
+    tienePermiso = False
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    body={
-        "url":endPoint,
-        "metodo":metodo
+    body = {
+        "url": endPoint,
+        "metodo": metodo
     }
-    response = requests.get(url,json=body, headers=headers)
+    response = requests.get(url, json=body, headers=headers)
     try:
-        data=response.json()
-        if("_id" in data):
-            tienePermiso=True
+        data = response.json()
+        if ("_id" in data):
+            tienePermiso = True
     except:
         pass
     return tienePermiso
-###################################################################################
-@app.route("/estudiantes",methods=['GET'])
-def getEstudiantes():
+
+###########################################
+########## METODOS PARA USUARIOS ##########
+###########################################
+
+@app.route("/usuarios", methods=['GET'])
+def getUsuarios():
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    url = dataConfig["url-backend-academic"] + '/estudiantes'
+    url = dataConfig["url-backend-security"] + '/usuarios'
     response = requests.get(url, headers=headers)
     json = response.json()
     return jsonify(json)
-@app.route("/estudiantes",methods=['POST'])
-def crearEstudiante():
-    data = request.get_json()
+
+@app.route("/usuarios/<string:id>", methods=['GET'])
+def getUsuario(id):
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    url = dataConfig["url-backend-academic"] + '/estudiantes'
-    response = requests.post(url, headers=headers,json=data)
-    json = response.json()
-    return jsonify(json)
-@app.route("/estudiantes/<string:id>",methods=['GET'])
-def getEstudiante(id):
-    headers = {"Content-Type": "application/json; charset=utf-8"}
-    url = dataConfig["url-backend-academic"] + '/estudiantes/'+id
+    url = dataConfig["url-backend-security"] + '/usuarios/'+id
     response = requests.get(url, headers=headers)
     json = response.json()
     return jsonify(json)
-@app.route("/estudiantes/<string:id>",methods=['PUT'])
-def modificarEstudiante(id):
+
+@app.route("/usuarios", methods=['POST'])
+def crearUsuario():
     data = request.get_json()
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    url = dataConfig["url-backend-academic"] + '/estudiantes/'+id
+    url = dataConfig["url-backend-security"] + '/usuarios'
+    response = requests.post(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route("/usuarios/<string:id>", methods=['PUT'])
+def modificarUsuario(id):
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-security"] + '/usuarios/'+id
     response = requests.put(url, headers=headers, json=data)
     json = response.json()
     return jsonify(json)
-@app.route("/estudiantes/<string:id>",methods=['DELETE'])
-def eliminarEstudiante(id):
+
+@app.route("/usuarios/<string:id>", methods=['DELETE'])
+def eliminarUsuario(id):
     headers = {"Content-Type": "application/json; charset=utf-8"}
-    url = dataConfig["url-backend-academic"] + '/estudiantes/' + id
+    url = dataConfig["url-backend-security"] + '/usuarios/' + id
     response = requests.delete(url, headers=headers)
+    return jsonify(response.text)
+
+###########################################
+########## METODOS PARA PARTIDOS ##########
+###########################################
+
+@app.route("/partidos", methods=['GET'])
+def getPartidos():
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-resultados"] + '/partidos'
+    response = requests.get(url, headers=headers)
     json = response.json()
     return jsonify(json)
-@app.route("/",methods=['GET'])
+
+@app.route("/partidos/<string:id>", methods=['GET'])
+def getPartido(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-resultados"] + '/partidos/'+ id
+    response = requests.get(url, headers=headers)
+    json = response.json()
+    return jsonify(json)
+
+@app.route("/partidos", methods=['POST'])
+def crearPartido():
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-resultados"] + '/partidos'
+    response = requests.post(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=['PUT'])
+def modificarPartido(id):
+    data = request.get_json()
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-resultados"] + '/partidos/'+id
+    response = requests.put(url, headers=headers, json=data)
+    json = response.json()
+    return jsonify(json)
+
+@app.route("/partidos/<string:id>", methods=['DELETE'])
+def eliminarPartido(id):
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    url = dataConfig["url-backend-resultados"] + '/partidos/' + id
+    response = requests.delete(url, headers=headers)
+    return jsonify(response.text)
+
+
+@app.route("/", methods=['GET'])
 def test():
     json = {}
-    json["message"]="Server running ..."
+    json["message"] = "Server running ..."
     return jsonify(json)
+
 
 def loadFileConfig():
     with open('config.json') as f:
         data = json.load(f)
     return data
-if __name__=='__main__':
-    dataConfig = loadFileConfig()
-    print("Server running : "+"http://"+dataConfig["url-backend"]+":" + str(dataConfig["port"]))
-    serve(app,host=dataConfig["url-backend"],port=dataConfig["port"])
 
+
+if __name__ == '__main__':
+    dataConfig = loadFileConfig()
+    print("Server running : "+"http://" +
+          dataConfig["url-backend"]+":" + str(dataConfig["port"]))
+    serve(app, host=dataConfig["url-backend"], port=dataConfig["port"])
